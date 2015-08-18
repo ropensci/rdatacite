2d348945944d6cc7cc8e072e1d19f49189ddffec
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-18 07:50:07 -0700

    tweaks in zzz helper fxns

--- R/zzz.r
+++ R/zzz.r
@@ -1,3 +1,15 @@
-dc_compact <- function (l) Filter(Negate(is.null), l)
+dc_compact <- function(l) Filter(Negate(is.null), l)
 
 dc_base <- function() "http://search.datacite.org/api"
+
+dc_oai_base <- function() "http://oai.datacite.org/oai"
+
+pluck <- function(x, name, type) {
+  if (missing(type)) {
+    lapply(x, "[[", name)
+  } else {
+    vapply(x, "[[", name, FUN.VALUE = type)
+  }
+}
+
+last <- function(x) x[length(x)][[1]]
