52bc7546525db9c195596305c58aa0e471dce645
Scott Chamberlain <myrmecocystus@gmail.com>
2017-11-14 09:47:12 -0800

    fix dc_search tests for changes in solr endpt returned fields

--- tests/testthat/test-dc_search.R
+++ tests/testthat/test-dc_search.R
@@ -9,34 +9,32 @@ test_that("dc_search basic functionality works", {
   # basic search
   #bb <- dc_search(q = "laser", rows = 2, verbose = FALSE)
   # specify fields to get back
-  bb <- dc_search(q = "laser", fl=c('doi','publicationYear'),
+  bb <- dc_search(q = "data", fl=c('doi','updated'),
     rows=2)
   # search a specific field
-  cc <- dc_search(q = "subject:geology", fl=c('doi','subject'),
+  cc <- dc_search(q = "datacentre:global", fl=c('doi','state'),
     rows=2)
 
   expect_is(aa, "tbl_df")
   expect_is(aa$minted, "character")
-  expect_is(aa$subject, "character")
   expect_lt(20, NCOL(aa))
 
   expect_is(bb, "tbl_df")
-  expect_named(bb, c('doi', 'publicationYear'))
+  expect_named(bb, c('updated', 'doi'))
   expect_equal(NROW(bb), 2)
 
   expect_is(cc, "tbl_df")
-  expect_named(cc, c('doi', 'subject'))
+  expect_named(cc, c('state', 'doi'))
   expect_equal(NROW(cc), 2)
 })
 
 test_that("dc_search works w/ csv output", {
   skip_on_cran()
 
-  aa <- dc_search(q = 'wind', fl=c('doi','title'), wt='csv')
-  bb <- dc_search(q = 'wind', fl=c('doi','title'))
+  aa <- dc_search(q = '*:*', fl=c('doi','title'), wt='csv')
+  bb <- dc_search(q = '*:*', fl=c('doi','title'))
 
   expect_is(aa, "tbl_df")
-  expect_named(aa, c('doi', 'title'))
 
   # csv and json output differ, some encoding problem likely
   expect_false(identical(aa, bb))
