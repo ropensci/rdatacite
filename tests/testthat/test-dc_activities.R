557b40830fe72d28f0b5d4ea9c1bef2691d76977
Scott Chamberlain <myrmecocystus@gmail.com>
2020-01-02 14:24:31 -0800

    preserve_exact_body_bytes for some tests so they work on windows

--- tests/testthat/test-dc_activities.R
+++ tests/testthat/test-dc_activities.R
@@ -3,7 +3,7 @@ context("dc_activities")
 test_that("dc_activities works", {
   vcr::use_cassette("dc_activities", {
     aa <- dc_activities(limit = 10)
-  })
+  }, preserve_exact_body_bytes = TRUE)
   expect_is(aa, "dc")
   expect_equal(sort(names(aa)), c("data", "included", "links", "meta"))
   expect_is(aa$data, 'data.frame')
