475c47bef625e9e12d2571a610c2ab57eadc97c5
Scott Chamberlain <myrmecocystus@gmail.com>
2020-03-10 16:46:43 -0700

    add dc_caching/dc_midden manual file - export dc_midden object

---
+++ R/midden.R
@@ -0,0 +1,14 @@
+#' Caching control
+#' 
+#' @export
+#' @name dc_caching
+#' @alias dc_midden
+#' @examples \dontrun{
+#' dc_midden
+#' dc_midden$cache
+#' 
+#' # set a different cache directory
+#' dc_midden$init(full_path = "some/route")
+#' dc_midden$cache
+#' }
+"dc_midden"
